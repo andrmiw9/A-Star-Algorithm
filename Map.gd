cbf9dbc3878649b6e0a0f4cb566315abb9a9867a
andrmiw9 <34866926+andrmiw9@users.noreply.github.com>
2022-05-24 15:14:52 +0300

    V4 Pre-final (mb final)

--- Map.gd
+++ Map.gd
@@ -10,14 +10,17 @@ var OpenQ := []		# priority queue of tiles, that will be used to find a way, the
 var needsClearing := false
 var Cells := []		# all Cells objects in TileMap, indexes are same with usedC
 var isWorking := false
-# LIFO
+
+var iterCount := 0
 
 
 class Cell:
+	var isClosed := false
 	var pos := Vector2.ZERO
 	var CheapestPath = 1000000
 #	var neighbours
 	var Score = 1000000
+	var h = -1
 	var CameFrom = null
 
 	func _init(Position) -> void:
@@ -26,12 +29,12 @@ class Cell:
 
 
 func _ready() -> void:
-	var toggleDebug := false
+	var toggleDebug := true
 	if(toggleDebug):
 		OS.window_size = Vector2(1075,640)
 		OS.window_position = Vector2(0,1200)
 		OS.current_screen = 1
-	var debugNout := true
+	var debugNout := false
 	if(debugNout):
 		OS.window_size = Vector2(800, 500)
 		OS.window_position = Vector2(0,540)
@@ -86,75 +89,88 @@ func StartSearching(endPos) -> void:
 	print("\nSTART\n")
 	while !OpenQ.empty():			# Main loop
 		# f(n) = g(n) + h(n), n - next node to path, g(n) - уже пройденный путь, h - эвристика
+		print("\nWhile iteration")
 		curCell = FindMinScoreInQ()[1]
 		if(curCell.pos == endPos):
 			print("Find the EndPos!")
-			return
-		
+			break
+		print("Now looking at:", curCell.pos)
 		OpenQ.remove(OpenQ.find(curCell))
 		set_cellv(curCell.pos, 6)
+		curCell.isClosed = true
 		
 		var points_nearBy = [curCell.pos + Vector2.UP, curCell.pos + Vector2.RIGHT, curCell.pos + Vector2.DOWN, curCell.pos + Vector2.LEFT]
 		for NearPo in points_nearBy:					# берем cell
-			if(!get_used_cells().has(NearPo)):						# выходим если клетка не тайлмэпа
+			if(!get_used_cells().has(NearPo)):			# пропускаем клетку если она не принадлежит тайлмэпу
 				continue
 			var NearCell = FindCellByPos(NearPo)		# ищем сам селл
+			if(NearCell.isClosed):						# пропускаем клетку, если мы уже рассматривали её
+				continue
 #			var NearCell = Cells[usedC.find(NearPo)]# берем связанный Cell object
 #			NearCell.pos = NearPo
 			print("NearCellPos: ", NearCell.pos)
-			var put = curCell.CheapestPath + (NearCell.pos - curCell.pos).length()	# cчитаем кратчайший путь до соседа
+			var put = curCell.CheapestPath + 1	# cчитаем кратчайший путь до соседа (NearCell.pos - curCell.pos).length() будет всегда 1
 			print("Put: ", put)
 			if(put < NearCell.CheapestPath):			# если он оказался меньше
 				NearCell.CameFrom = curCell			# ставим соседу откуда пришёл
 				NearCell.CheapestPath = put			# задаём сам путь
-				NearCell.Score = put + h(NearPo, endPos)		# функция оценки в очереди
+				NearCell.h = ceil(h(NearPo, endPos))	# 68 for ceil, 75 for floor, 72 for nothing, 73 for round, 73 for stepify for 1.
+				NearCell.Score = put + NearCell.h		# функция оценки в очереди
 				
 				if(!OpenQ.has(NearCell)):
 					OpenQ.append(NearCell)
 				
 			
 #		print("OpenQ after appending: ", OpenQ)
-		print("OpenQPos after checking neighbours:")
-		PrintOpenQPos()
+		print("OpenQ after checking neighbours:")
+		PrintOpenQ()
 #		PrintOpenQScores()
 #		OpenQ.sort_custom(CustomSorter, "sort_ascending")
 #		print("OpenQ after sorting: ", OpenQ)
 #		PrintOpenQScores()
 		CustomDraw()
-		yield(get_tree().create_timer(0.3), "timeout")
+		iterCount += 1
+		yield(get_tree().create_timer(0.1), "timeout")
 #		update()
-	print("While finished!")
+	print("\nWhile finished!")
+	print("Number of iterations: ", iterCount)
 	ReconstructWay()
+	needsClearing = true
 	pass
-
-
-func PrintOpenQPos() -> void:
-	print("\nPrintOpenQPos:")
-	for cell in OpenQ:
-		print(cell.pos)
-
-
-func PrintOpenQScores() -> void:
-	print("\nPrintOpenQScores:")
-	for cell in OpenQ:
-		print(cell.Score)
-
-
-class CustomSorter:
-	static func sort_ascending(a, b):
-		if a.pos < b.pos:
-			return true
-		return false
 
 
 func ReconstructWay() -> void:
 	pass
 
 
+# Prints--------------------------------------
+func PrintOpenQ() -> void:
+	print("\nPrintOpenQPosAndScores:")
+	for cell in OpenQ:
+		print(cell.pos, "; ", cell.Score,"; ", cell.h)
+
+func PrintOpenQPos() -> void:
+	print("\nPrintOpenQPos:")
+	for cell in OpenQ:
+		print(cell.pos)
+
+func PrintOpenQScores() -> void:
+	print("\nPrintOpenQScores:")
+	for cell in OpenQ:
+		print(cell.Score)
+# --------------------------------------------
+
+#class CustomSorter:
+#	static func sort_ascending(a, b):
+#		if a.pos < b.pos:
+#			return true
+#		return false
+
+
 # Эвристическая функция оценки расстояния до цели (просто считает длину вектора из переданной точки до финальной)
 func h(pos, endpos) -> float:
-	print("h will return: ",(pos-endpos).length())
-	return (pos - endpos).length()
+	print("h will return: ",(pos - endpos).length())
+	return (pos - endpos).length()		# возвращает длину в клетках до цели
 
 
 func FindCellByPos(pos) -> Cell:
